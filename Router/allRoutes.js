3265fc5b7637243a1023aebb9c639c31d3a06f62
Cédric DUMEAUX <c_dumeaux@outlook.fr>
2024-07-29 17:38:03 +0200

    Création de la page d'inscription

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -6,6 +6,7 @@ export const allRoutes = [
     new Route("/galerie", "La galerie", "/pages/galerie.html"),
     new Route("/carte", "La carte", "/pages/carte.html"),
     new Route("/signin", "Connexion", "/pages/signin.html"),
+    new Route("/signup", "Inscription", "/pages/signup.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
